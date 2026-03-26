3f8fa181e68651c6443e032257afbe83d82e6be6
genecarles <genecarles@gmail.com>
2026-03-26 19:14:26 +0100

    Create error.js

---
+++ error.js
@@ -0,0 +1,3 @@
+// Aquest fitxer controla visualment la pàgina d'error 404
+document.title = "404 Not Found";
+document.body.innerHTML = '<img src="https://raw.githubusercontent.com/altervector/cadinamics/main/Icon/404.png" style="max-width:100%; height:auto;">';
