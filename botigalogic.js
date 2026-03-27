92b23ee893fac9c555344b02c900bf98e383d037
genecarles <genecarles@gmail.com>
2026-03-27 18:17:18 +0100

    REN PICS CAT

--- botigalogic.js
+++ botigalogic.js
@@ -3,24 +3,24 @@ const htmlBotiga = `
 <div class="bloc-imatges">
     <div class="columna-esquerra">
         <a href="https://cadialimentacio.netlify.app/principal.html?cat=ver" class="enllac-imatge-fix"> 
-            <img src="https://altervector.github.io/cadinamics/images/Verdura.webp" alt="Verdures" class="imatge-petita">
+            <img src="https://altervector.github.io/cadinamics/images/PICATVer.webp" alt="Verdures" class="imatge-petita">
             <span class="etiqueta-categoria">Verdures</span>
         </a>
         <a href="https://cadialimentacio.netlify.app/principal.html?cat=pre" class="enllac-imatge-fix">
-            <img src="https://altervector.github.io/cadinamics/images/Croquetas.webp" alt="Croquetes" class="imatge-petita">
+            <img src="https://altervector.github.io/cadinamics/images/PICATPre.webp" alt="Croquetes" class="imatge-petita">
             <span class="etiqueta-categoria">Precuinats</span>
         </a>
     </div>
     <div class="imatge-dreta">
         <a href="https://cadialimentacio.netlify.app/principal.html?cat=px" class="enllac-imatge-fix">
-            <img src="https://altervector.github.io/cadinamics/images/Peix.webp" alt="Peix i Marisc" class="imatge-gran">
+            <img src="https://altervector.github.io/cadinamics/images/PICATPx.webp" alt="Peix i Marisc" class="imatge-gran">
             <span class="etiqueta-categoria">Peix i Marisc</span>
         </a>
     </div>
 </div>
 <div class="bloc-imatge-inferior">
     <a href="https://cadialimentacio.netlify.app/principal.html?cat=alt" class="enllac-imatge-fix item-horizontal">
-        <img src="https://altervector.github.io/cadinamics/images/postres.webp" alt="Postres i Altres" class="imatge-petita">
+        <img src="https://altervector.github.io/cadinamics/images/PICATAlt.webp" alt="Postres i Altres" class="imatge-petita">
         <span class="etiqueta-categoria">Postres i Altres</span>
     </a>
 </div>
