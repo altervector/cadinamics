a0c4f0106920e2665f8e90e9bc71db022155ec0f
genecarles <genecarles@gmail.com>
2026-04-18 16:03:30 +0200

    Fix Instagram handle in enllacos.js

--- enllacos.js
+++ enllacos.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
         
         <div class="bloc-enllacos">
             <p><a href="https://altervector.github.io/cadinamics/1PROMO.pdf"><img src="https://altervector.github.io/cadinamics/Icon/Icopdf.png" alt="Pdf" class="icona-app">Descarrega el catàleg complet (PDF)</a></p>
-            <p><a href="https://www.instagram.com/carlescadi" target="_blank"><img src="https://altervector.github.io/cadinamics/Icon/Icoinsta.png" alt="Instagram" class="icona-app">Instagram: @cadicarles</a></p>
+            <p><a href="https://www.instagram.com/carlescadi" target="_blank"><img src="https://altervector.github.io/cadinamics/Icon/Icoinsta.png" alt="Instagram" class="icona-app">Instagram: @carlescadi</a></p>
             <p><a href="https://wa.me/34690091388"><img src="https://altervector.github.io/cadinamics/Icon/icowhats.png" alt="Whatsapp" class="icona-app">Contacta per WhatsApp</a></p>
             <p><a href="mailto:cadimobil2@gmail.com"> <img src="https://altervector.github.io/cadinamics/Icon/Icomail.png" alt="e-mail" class="icona-app">Envia un correu</a></p>
         </div>
