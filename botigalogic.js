3ee7374167434b2edabd7ecd5e778877c9d9f2b3
genecarles <genecarles@gmail.com>
2026-04-23 06:02:27 +0200

    Update links from Netlify to Pages.dev

--- botigalogic.js
+++ botigalogic.js
@@ -2,24 +2,24 @@ const htmlBotiga = `
 
 <div class="bloc-imatges">
     <div class="columna-esquerra">
-        <a href="https://cadialimentacio.netlify.app/principal.html?cat=ver" class="enllac-imatge-fix"> 
+        <a href="https://cadialimentacio.pages.dev/principal.html?cat=ver" class="enllac-imatge-fix"> 
             <img src="https://altervector.github.io/cadinamics/images/PICATVer.webp" alt="Verdures" class="imatge-petita">
             <span class="etiqueta-categoria">Verdures</span>
         </a>
-        <a href="https://cadialimentacio.netlify.app/principal.html?cat=pre" class="enllac-imatge-fix">
+        <a href="https://cadialimentacio.pages.dev/principal.html?cat=pre" class="enllac-imatge-fix">
             <img src="https://altervector.github.io/cadinamics/images/PICATPre.webp" alt="Croquetes" class="imatge-petita">
             <span class="etiqueta-categoria">Precuinats</span>
         </a>
     </div>
     <div class="imatge-dreta">
-        <a href="https://cadialimentacio.netlify.app/principal.html?cat=px" class="enllac-imatge-fix">
+        <a href="https://cadialimentacio.pages.dev/principal.html?cat=px" class="enllac-imatge-fix">
             <img src="https://altervector.github.io/cadinamics/images/PICATPx.webp" alt="Peix i Marisc" class="imatge-gran">
             <span class="etiqueta-categoria">Peix i Marisc</span>
         </a>
     </div>
 </div>
 <div class="bloc-imatge-inferior">
-    <a href="https://cadialimentacio.netlify.app/principal.html?cat=alt" class="enllac-imatge-fix item-horizontal">
+    <a href="https://cadialimentacio.pages.dev/principal.html?cat=alt" class="enllac-imatge-fix item-horizontal">
         <img src="https://altervector.github.io/cadinamics/images/PICATAlt.webp" alt="Postres i Altres" class="imatge-petita">
         <span class="etiqueta-categoria">Postres i Altres</span>
     </a>
